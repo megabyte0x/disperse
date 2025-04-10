016d33e5896ef1c8792c17e3c2e5427facdc71a8
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-04-10 15:59:27 +0530

    removed onchain minikit

--- lib/constants.ts
+++
@@ -1,4 +0,0 @@
-import Disperse from "@/contracts/out/Disperse.sol/Disperse.json";
-
-export const CONTRACT_ABI = Disperse.abi;
-export const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000000";
