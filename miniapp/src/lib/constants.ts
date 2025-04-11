24999e6d39e3b57891efcb298e38decef745b6de
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-04-11 15:28:26 +0530

    minor fixes

--- miniapp/src/lib/constants.ts
+++ miniapp/src/lib/constants.ts
@@ -1,5 +1,5 @@
 import Disperse from "../../contracts/out/Disperse.sol/Disperse.json";
 
 // This should be replaced with the actual deployed contract address for the Base network
-export const CONTRACT_ADDRESS = "0xD152f549545093347A162Dce210e7293f1452150"; // Example Disperse contract on Base
+export const CONTRACT_ADDRESS = "0x7b6D4472B4Cd6deCb69029c295b7d40e3c8E3384"; // Example Disperse contract on Base
 export const CONTRACT_ABI = Disperse.abi;
